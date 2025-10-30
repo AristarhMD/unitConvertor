2cd49ff1ca1a9cac28fed3e8ad53e07d1f4c4684
Victor Nani <victor.nani1996@gmail.com>
2025-10-30 10:11:43 +0200

    Final version
    Was putted:
     - logic of calculation for all 3 type;
     - toggle icons for light / dark mode
    - applying of the styles for this modes

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 const inputEl = document.querySelector('input[type="number"]');
 const convertBtn = document.querySelector(".action-btn");
 
+// Mode toggler container
+const modeContainer = document.querySelector(".mode-toggler");
+
 // Select paragraph to modify
 const lengthPar = document.querySelector(".lenght");
 const volumePar = document.querySelector(".volume");
-const MassPar = document.querySelector(".mass");
+const massPar = document.querySelector(".mass");
 
-// Button click event listener
+// Event listener for change of the mode
+modeContainer.addEventListener("click", toggleMode);
+
+// function for toogling mode element visibility and element styles
+function toggleMode() {
+  document
+    .querySelectorAll(".mode")
+    .forEach((mode) => mode.classList.toggle("hide"));
+  document
+    .querySelector(".converting-data")
+    .classList.toggle("invertion-converting-data");
+
+  document
+    .querySelectorAll(".metric-data")
+    .forEach((el) => el.classList.toggle("invertion-metric-data"));
+  document
+    .querySelectorAll("h2")
+    .forEach((el) => el.classList.toggle("invertion-h2"));
+  document
+    .querySelectorAll(".fs")
+    .forEach((el) => el.classList.toggle("invertion-fs"));
+}
+
+// Convert button click event listener
 convertBtn.addEventListener("click", function () {
   inputData = Number(inputEl.value);
   if (!inputData) return;
-  lengthPar.textContent = `${inputData} meters  = ${inputData * 3.281} feet`;
+
+  // Calculating meter to feet / feet to meter and update UI
+  const meterToFeet = (inputData * 3.2808).toFixed(3);
+  const feetToMetr = (inputData * 0.3048).toFixed(3);
+
+  lengthPar.textContent = `${inputData} meters  = ${meterToFeet} feet | ${inputData} feet = ${feetToMetr} meters `;
+
+  // Calculating liters to gallons / gallons to liters and update UI
+  const litersToGallons = (inputData * 0.264172).toFixed(3);
+  const gallonsToLiters = (inputData * 3.78541).toFixed(3);
+
+  volumePar.textContent = `${inputData} liters = ${litersToGallons} gallons | ${inputData} = ${gallonsToLiters} liters`;
+
+  // Calculating kilos to pounds / pounds to kilos and update UI
+  const kilosToPounds = (inputData * 2.20462).toFixed(3);
+  const poundsToKilos = (inputData * 0.453592).toFixed(3);
+
+  massPar.textContent = `${inputData} kilos = ${kilosToPounds} pounds | ${inputData} pounds = ${poundsToKilos} kilos`;
 });
-
-/*
-
-1 meter = 3.281 feet
-
-1 liter = 0.264 gallon
-
-1 kilogram = 2.204 pound
-
-*/
